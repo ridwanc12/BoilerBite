89f7ceb860566a6e6e0186d97ca4120d14f65772
Ridwan Chowdhury <rchowdhury1212@gmail.com>
2020-02-14 14:23:57 -0500

    Began work on api request for individual items

--- BoilerBiteMobile/BoilerBiteMobile/api_test.playground/Contents.swift
+++ BoilerBiteMobile/BoilerBiteMobile/api_test.playground/Contents.swift
@@ -9,9 +9,24 @@ var requestLocation = URL(string: address)
 
 URLSession.shared.dataTask(with: requestLocation!) { (data, response, error) in
     do {
-        print(response)
+        print(data)
     } catch {
         print("There was an error in the api request")
         print(error)
     }
 }.resume()
+
+// URL for example item api request
+// https://api.hfs.purdue.edu/menus/v2/items/6c883ba0-e283-4086-ab01-e181a6615435
+
+var address2 = "https://api.hfs.purdue.edu/menus/v2/items/6c883ba0-e283-4086-ab01-e181a6615435"
+var requestLocation2 = URL(string: address2)
+
+URLSession.shared.dataTask(with: requestLocation2!) { (data, response, error) in
+    do {
+        print(data)
+    } catch {
+        print("There was an error in the api request")
+        print(error)
+    }
+    }.resume()
