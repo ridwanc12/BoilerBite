8c7029fd360076928877cbaab0fdebc77ae72957
Uday <uchaudh@purdue.edu>
2020-02-16 13:22:32 -0500

    Spent two hours on learning git and setting up branch

---
+++ BoilerBite/BoilerBite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoilerBite
+//
+//  Created by Uday Chaudhary on 16/02/20.
+//  Copyright © 2020 Uday Chaudhary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
