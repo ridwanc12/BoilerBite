cfed6efcbddbb3ccff577b69ad0286b4f513a6e4
Uday <uchaudh@purdue.edu>
2020-02-26 19:12:17 -0500

    Database request initial test

--- BoilerBite/BoilerBite/MainViewController.swift
+++ BoilerBite/BoilerBite/MainViewController.swift
@@ -39,6 +39,83 @@ class MainViewController: UIViewController {
         // Do not need to do anything
     }
     
+    
+    let URL_SAVE_TEAM = URL(string: "http://10.192.122.81/MyWebService/api/createteam.php")
+       
+       //TextFields declarations
+       @IBOutlet weak var textFieldName: UITextField!
+       @IBOutlet weak var textFieldMember: UITextField!
+    
+       // the content inside UC
+       //Button action method
+       @IBAction func buttonSave(sender: UIButton) {
+        
+        
+        //created NSURL
+        //let requestURL = NSURL(string: URL_SAVE_TEAM)
+        
+        //creating NSMutableURLRequest
+        let request = NSMutableURLRequest()
+        
+        //setting the method to post
+        request.httpMethod = "POST"
+        
+        //getting values from text fields
+//        let username = textFieldName.text
+//        let password = textFieldMember.text
+        let username = "uday"
+        let password = "password"
+        
+        // name = username, memeberCount=password
+        //creating the post parameter by concatenating the keys and values from text field
+        let postParameters = "username="+username+"&password="+password;
+        
+        //adding the parameters to request body
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        
+        
+        //creating a task to send the post request
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            data, response, error in
+            
+            if error != nil{
+                print("error is \(String(describing: error))")
+                return;
+            }
+        
+            //parsing the response
+            do {
+                //converting resonse to NSDictionary
+                let myJSON =  try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                
+                //parsing the json
+                if let parseJSON = myJSON {
+                    
+                    //creating a string
+                    var msg : String!
+                    
+                    //getting the json response
+                    msg = parseJSON["message"] as! String?
+                    
+                    //printing the response
+                    print(msg!)
+                    
+                }
+            } catch {
+                print(error)
+            }
+            
+        }
+        //executing the task
+        task.resume()
+       }
+    // added by UC
+    
+    // added by UC
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
     /*
     // MARK: - Navigation
