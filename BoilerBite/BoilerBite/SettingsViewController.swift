5623dacda4a43c6c10dbaed276b83ccaf2616483
Isha Mahadalkar <ishamahadalkar03@gmail.com>
2020-03-30 03:00:46 +0530

    Created Custom View Controllers for Settings Page and implemented Logout Segue

---
+++ BoilerBite/BoilerBite/SettingsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsViewController.swift
+//  BoilerBite
+//
+//  Created by Isha Mahadalkar on 3/30/20.
+//  Copyright © 2020 Isha Mahadalkar. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    // Outlets for the Delete Account and Logout Button
+    
+    @IBOutlet weak var deleteAccountButton: UIButton!
+    @IBOutlet weak var logoutButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
