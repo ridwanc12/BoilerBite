6a09198c874c677bf83dfdb109e82e637a975ad7
csuser <csuser@csusers-MacBook-Pro.local>
2020-03-31 12:38:41 -0400

    Modify printed response in deleteUser

--- BoilerBite/BoilerBite/ServerRequest.swift
+++ BoilerBite/BoilerBite/ServerRequest.swift
@@ -31,7 +31,7 @@ func insertUser(name: String, mail: String, pass: String){
             return
         }
 
-        print("response = \(String(describing: response))")
+        //print("response = \(String(describing: response))")
 
         let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
         print("responseString = \(String(describing: responseString))")
@@ -40,7 +40,7 @@ func insertUser(name: String, mail: String, pass: String){
 }
 
 // Function to test php script to delete user from database
-func deleteUser(name: String, pass: String){
+func deleteUser(name: String, pass: String) {
 //    let name = "Isha"
 //    let pass = "isha"
     let link = "https://boilerbite.000webhostapp.com/php/deleteUser.php"
@@ -49,6 +49,7 @@ func deleteUser(name: String, pass: String){
     // Send values to php script
     let postString = "userName=\(name)&pass=\(pass)"
     request.httpBody = postString.data(using: String.Encoding.utf8)
+    
     
     let task = URLSession.shared.dataTask(with: request as URLRequest) {
         data, response, error in
@@ -59,14 +60,19 @@ func deleteUser(name: String, pass: String){
         }
 
         let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue) as String?
-        if (responseString?.contains("User deletion failed."))! {
-            print("user not deleted")
+        let response = String(describing: responseString)
+        print(response)
+        if (response.contains("User not in database.")) {
+            print("No such user")
+        } else if (response.contains("password")){
+            print("Incorrect password")
         } else {
-            print("deleted")
+            print("User deleted")
         }
-        //print("responseString = \(String(describing: responseString))")
+        //print("responseString = ")
     }
     task.resume()
+    
 }
 
 // Can use functions like the following
