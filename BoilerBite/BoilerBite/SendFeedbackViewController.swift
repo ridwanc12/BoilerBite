1b24f575c3892d9c1e56d4f13ecc961743de5bda
Isha Mahadalkar <ishamahadalkar03@gmail.com>
2020-04-24 00:00:40 +0530

    External functions for Sending Feedback and setting global variables

--- BoilerBite/BoilerBite/SendFeedbackViewController.swift
+++ BoilerBite/BoilerBite/SendFeedbackViewController.swift
@@ -15,12 +15,53 @@ class SendFeedbackViewController: UIViewController {
     
     // Function for when the "Send" button is pressed
     @IBAction func sendTapped(_ sender: UIButton) {
+        let feedback: String = feedbackText.text ?? ""
+        send_feedback(feedback: feedback)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func send_feedback(feedback: String) {
+        let semaphore = DispatchSemaphore (value: 0)
+
+        //let username: String = usernameField.text ?? ""
+        //let password: String = passwordField.text ?? ""
+        
+        // user changed to username
+        //let username = "sssssss"
+        //print("calories: " ,calories)
+        //print("username: " ,username)
+        
+        //let userr:String = String(global_username ?? "0") ?? "ss"
+        //let calories: Int = Int(caloriesField.text ?? "0") ?? 0
+        
+        
+        let urlString = String(format: "http://boilerbite.000webhostapp.com/php/mail.php?feedback=%@", feedback)
+        /*
+         var request = URLRequest(url: URL(string: urlString)!,timeoutInterval: Double.infinity)
+         */
+         
+        let r = URL(string: urlString)!
+        print(r)
+         var request = URLRequest(url: URL(string: urlString)!,timeoutInterval:       Double.infinity)
+        
+        request.httpMethod = "POST"
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            return
+          }
+          print(String(data: data, encoding: .utf8)!)
+          semaphore.signal()
+        }
+
+        task.resume()
+        semaphore.wait()
     }
     
 
