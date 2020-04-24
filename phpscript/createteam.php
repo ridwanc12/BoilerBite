a00b24665b712d2761d447507f311edbf820e4a5
yang1556 <yang1556@purdue.edu>
2020-04-24 11:52:37 -0400

    add salt to hash

--- phpscript/createteam.php
+++ phpscript/createteam.php
@@ -39,7 +39,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 
         $response['error']=true;
         $response['message']='login unsuccessful';
-        $res = 1;
+        $res = 0;
     }
 
 }else{
